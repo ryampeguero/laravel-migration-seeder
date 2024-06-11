d473ff0c3bf84d2b3b7cb35f7f1ac59406940c26
ryampeguero <ryampeguero@gmail.com>
2024-06-11 20:57:04 +0200

    feat: added some records to the table

--- database/migrations/2024_06_11_181439_create_trains_table.php
+++ database/migrations/2024_06_11_181439_create_trains_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('azienda');
-            $table->string('capotreno');
+            $table->integer('id_capotreno')->unsigned();
             $table->string('stazione_partenza');
             $table->string('stazione_arrivo');
             $table->time('orario_partenza');
