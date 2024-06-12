0c8633de9c31c017113b367d55b72bc363bf7bf4
ryampeguero <ryampeguero@gmail.com>
2024-06-12 22:32:48 +0200

    feat: Added query comment

--- database/migrations/2024_06_11_181439_create_trains_table.php
+++ database/migrations/2024_06_11_181439_create_trains_table.php
@@ -20,6 +20,20 @@ return new class extends Migration
      */
     public function up(): void
     {
+        CREATE TABLE trains(
+            id INT NOT NULL AUTO_INCREMENT,
+            azienda VARCHAR(255),
+            id_capotreno INT UNSIGNED,
+            stazione_partenza VARCHAR(255),
+            stazione_partenza VARCHAR(255),
+            orario_partenza TIME,
+            orario_arrivo TIME,
+            codice_treno VARCHAR(255),
+            numero_carrozze TINYINT,
+            numero_passeggeri MEDIUMINT,
+            in_orario TINYINT(1),
+            cancellato TINYINT(1),
+            PRIMARY_KEY(id));
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->string('azienda')->nullable();
@@ -29,7 +43,7 @@ return new class extends Migration
             $table->time('orario_partenza')->nullable();
             $table->time('orario_arrivo')->nullable();
             $table->string('codice_treno')->nullable();
-            $table->integer('numero_carrozze')->nullable();
+            $table->tinyInteger('numero_carrozze')->nullable();
             $table->mediumInteger('numero_passeggeri')->nullable();
             $table->boolean('in_orario')->nullable();
             $table->boolean('cancellato')->nullable();
